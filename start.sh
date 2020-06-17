379c01fb5c123cf9449d4d78325be6d0c357e520
Peer De bakker <pde-bakk@f0r3s22.codam.nl>
2020-06-17 21:04:38 +0200

    wrsfdsf

--- start.sh
+++ start.sh
@@ -4,4 +4,17 @@
 
 minikube delete
 
-minikube start --driver=virtualbox
+minikube start --driver=virtualbox \
+--addons dashboard \
+--addons ingress
+
+minikube dashboard
+
+cd srcs
+
+docker build -t nginx nginx/
+
+# -k applies a whole folder full of JSON/YAML files
+# -f does a single one
+kubectl apply -k .
+#Apply a configuration to a resource by filename or stdin. The resource name must be specified.
