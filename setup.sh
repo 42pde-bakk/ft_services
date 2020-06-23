9bde223fcf691f7fecbc8598f5f8d6beafa20aa0
Peer De bakker <pde-bakk@f0r3s8.codam.nl>
2020-06-23 11:35:11 +0200

    MetalLB seemed to be clashing with the ingress plugin

--- setup.sh
+++ setup.sh
@@ -2,16 +2,14 @@
 # mkdir -p ~/goinfre/minikube
 # ln -s ~/goinfre/minikube ~/.minikube
 
-# open --background -a Docker
-
 minikube delete
 
 minikube start --vm-driver=virtualbox \
---extra-config=apiserver.service-node-port-range=1-35000
+--cpus=4 --memory 8192
 # --bootstrapper=kubeadm \
 # --extra-config=kubelet.authentication-token-webhook=true
 
-minikube addons enable ingress
+# make sure to not enable the ingress addon if you are working with MetalLB
 minikube addons enable dashboard
 
 echo "Eval..."
@@ -26,16 +24,21 @@ docker build -t nginx_alpine ./srcs/nginx/
 docker build -t influxdb_alpine ./srcs/influxdb
 docker build -t ftps_alpine --build-arg IP=${IP} ./srcs/ftps/
 docker build -t grafana_alpine ./srcs/grafana
-docker build -t wordpress_alpine ./srcs/wordpress --build-arg IP=${IP}
+docker build -t wordpress_alpine ./srcs/wordpress
 
 sleep 5
 kubectl apply -k ./srcs/
+sleep 1
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+# sleep 1
+# kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
 # ./apply.sh
 
 # echo "\n\nHier: http://$MINIKUBE_IP"
-echo "Opening the network in your browser..."
-open http://$MINIKUBE_IP
-
+printf "Opening http://$MINIKUBE_IP in your browser...\n"
+open http://$MINIKUBE_IP # this wont work, you need the correct port:
+minikube service nginx --url
 # minikube dashboard
+
 # To enter terminal:
 # kubectl run -i --tty busybox --image=busybox --restart=Never -- sh 
--- srcs/wordpress/wp-config.php
+++ srcs/wordpress/wp-config.php
@@ -32,8 +32,8 @@ define( 'DB_PASSWORD', 'password' );
 /** MySQL hostname */
 define( 'DB_HOST', 'mysql-service' );
 
-define('WP_HOME', 'http://192.168.99.185:5050' );
-define('WP_SITEURL', 'http://192.168.99.185:5050' );
+define('WP_HOME', 'http://192.168.99.209:5050' );
+define('WP_SITEURL', 'http://192.168.99.209:5050' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
