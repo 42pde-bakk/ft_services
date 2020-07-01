ab70b93504c8222038e08a3388c061aae55b85f7
Peer De bakker <pde-bakk@f1r2s13.codam.nl>
2020-07-01 18:57:33 +0200

    using nodePort now

--- cleanup.sh
+++ cleanup.sh
@@ -11,54 +11,40 @@ White='\033[0;37m'        # White
 
 echo "${Yellow}---------------------------- Secret ------------------------------${Color_Off}"
 kubectl delete secrets
-kubectl delete pvc --all --grace-period=0 --force 
-echo "\n"
+# kubectl delete pvc --all --grace-period=0 --force &
 
 echo "${Yellow}---------------------------- Nginx ------------------------------${Color_Off}"
 kubectl delete services nginx
 kubectl delete deployment nginx
 kubectl delete configMaps/nginx-config
-echo "\n"
 
-echo -e "${Yellow}--------------------------- Mysql -------------------------------${Color_Off}"
+echo "${Yellow}--------------------------- Mysql -------------------------------${Color_Off}"
 kubectl delete deployment/mysql
 kubectl delete services/mysql
 kubectl delete configMaps/mysql-config
-# kubectl delete pvc/mysql-pv-claim
-echo "\n"
 
 echo "${Yellow}-------------------------- Wordpress ---------------------------${Color_Off}"
 kubectl delete deployment/wordpress
 kubectl delete services wordpress
 kubectl delete configMaps/wordpress-config
-# kubectl delete pvc/wordpress-pv-claim
-echo "\n"
 
 echo "${Yellow}------------------------- Phpmyadmin ----------------------------${Color_Off}"
 kubectl delete deployment/phpmyadmin
 kubectl delete service/phpmyadmin
-# kubectl delete pvc/phpmyadmin-pv-claim
 kubectl delete configMaps/phpmyadmin-config
-echo "\n"
 
-echo -e "${Yellow}-------------------------- Grafana ------------------------------${Color_Off}"
+echo "${Yellow}-------------------------- Grafana ------------------------------${Color_Off}"
 kubectl delete deployment/grafana
 kubectl delete service/grafana
 kubectl delete configMaps/grafana-config
-echo "\n"
-
-echo -e "${Yellow}-------------------------- InfluxDB -----------------------------${Color_Off}"
+echo "${Yellow}-------------------------- InfluxDB -----------------------------${Color_Off}"
 kubectl delete deployment/influxdb
 kubectl delete service/influxdb
 kubectl delete configMaps/influxdb-config
 
-# kubectl delete pvc/influxdb-pvc
-echo "\n"
-
-echo -e "${Yellow}-------------------------- Telegraf -----------------------------${Color_Off}"
+echo "${Yellow}-------------------------- Telegraf -----------------------------${Color_Off}"
 kubectl delete deployment/telegraf
 kubectl delete service/telegraf
 kubectl delete configmap/telegraf-config
-echo "\n"
 
 echo "${Green}Cluster cleaned${Color_Off}"
--- setup.sh
+++ setup.sh
@@ -9,43 +9,46 @@ end=$'\e[0m'
 # mkdir -p ~/goinfre/minikube
 # ln -s ~/goinfre/minikube ~/.minikube
 
+:> errlog.txt
+:> log.log
 # minikube delete
-sh cleanup.sh
+sh cleanup.sh >> log.log 2>> errlog.txt
 
 minikube start	--vm-driver=virtualbox \
 				--cpus=2 --memory 3000 \
 				--bootstrapper=kubeadm	\
 				--extra-config=kubelet.authentication-token-webhook=true \
-				--extra-config=apiserver.service-node-port-range=3000-35000
+				--extra-config=apiserver.service-node-port-range=3000-35000 \
+				>> log.log 2>> errlog.txt
 
-minikube addons enable metallb
-echo "Eval..."
+minikube addons enable metallb >> log.log 2>> errlog.txt
+minikube addons enable default-storageclass >> log.log 2>> errlog.txt
+minikube addons enable storage-provisioner >> log.log 2>> errlog.txt
+echo "Done starting fam"
 eval $(minikube docker-env) # eval $(minikube -p minikube docker-env)
 export MINIKUBE_IP=$(minikube ip)
 printf "Minikube IP: ${MINIKUBE_IP}\n"
 
-# sed "s/MINIKUBE_IP/$MINIKUBE_IP/g" srcs/nginx/homepage-pde-bakk/beforesed.html > srcs/nginx/homepage-pde-bakk/index.html
+sed "s/MINIKUBE_IP/$MINIKUBE_IP/g" srcs/nginx/homepage-pde-bakk/beforesed.html > srcs/nginx/homepage-pde-bakk/index.html
 # sed "s/MINIKUBE_IP/$MINIKUBE_IP/g" srcs/wordpress/tmp.sh > srcs/wordpress/setup.sh
 
-kubectl apply -f ./srcs/metallb.yaml > configlog.txt
-echo "Building images..."
-echo "images errlog" > errlog.txt
-printf "Building ftps image:\t\t"
-docker build -t ftps_alpine ./srcs/ftps2 --build-arg IP=${IP} > /dev/null 2>>errlog.txt && printf "[${grn}OK${end}]\n" && kubectl apply -f ./srcs/ftps.yaml >> configlog.txt || printf "[${red}NO${end}]\n"
-printf "Building mysql image:\t\t"
-docker build -t mysql_alpine ./srcs/mysql > /dev/null 2>>errlog.txt && printf "[${grn}OK${end}]\n" && kubectl apply -f ./srcs/mysql.yaml >> configlog.txt || printf "[${red}NO${end}]\n"
-printf "Building wordpress image:\t"
-docker build -t wordpress_alpine ./srcs/wordpress > /dev/null 2>>errlog.txt && printf "[${grn}OK${end}]\n" && kubectl apply -f ./srcs/wordpress.yaml >> configlog.txt || printf "[${red}NO${end}]\n"
-printf "Building phpmyadmin image:\t"
-docker build -t phpmyadmin_alpine ./srcs/phpmyadmin > /dev/null 2>>errlog.txt && printf "[${grn}OK${end}]\n" && kubectl apply -f ./srcs/phpmyadmin.yaml >> configlog.txt || printf "[${red}NO${end}]\n"
-printf "Building influxdb image:\t"
-docker build -t influxdb_alpine ./srcs/influxdb2 >> /dev/null 2>>errlog.txt && printf "[${grn}OK${end}]\n" && kubectl apply -f ./srcs/influxdb.yaml >> configlog.txt || printf "[${red}NO${end}]\n"
-printf "Building grafana image:\t\t"
-docker build -t grafana_alpine ./srcs/grafana2 > /dev/null 2>>errlog.txt && printf "[${grn}OK${end}]\n" && kubectl apply -f ./srcs/grafana.yaml >> configlog.txt || printf "[${red}NO${end}]\n"
-printf "Building nginx image:\t\t"
-docker build -t nginx_alpine ./srcs/nginx > /dev/null 2>>errlog.txt && printf "[${grn}OK${end}]\n" && kubectl apply -f ./srcs/nginx.yaml >> configlog.txt || printf "[${red}NO${end}]\n"
+kubectl apply -f ./srcs/metallb.yaml >> log.log
+printf "Building and deploying ftps:\t\t"
+docker build -t ftps_alpine ./srcs/ftps2 --build-arg IP=${IP} > /dev/null 2>>errlog.txt && printf "[${grn}OK${end}]\n" || printf "[${red}NO${end}]\n"; kubectl apply -f ./srcs/ftps.yaml >> log.log 2>> errlog.txt
+printf "Building and deploying mysql:\t\t"
+docker build -t mysql_alpine ./srcs/mysql > /dev/null 2>>errlog.txt && printf "[${grn}OK${end}]\n" || printf "[${red}NO${end}]\n"; kubectl apply -f ./srcs/mysql.yaml >> log.log 2>> errlog.txt
+printf "Building and deploying wordpress:\t"
+docker build -t wordpress_alpine ./srcs/wordpress > /dev/null 2>>errlog.txt && printf "[${grn}OK${end}]\n" || printf "[${red}NO${end}]\n"; kubectl apply -f ./srcs/wordpress.yaml >> log.log 2>> errlog.txt
+printf "Building and deploying phpmyadmin:\t"
+docker build -t phpmyadmin_alpine ./srcs/phpmyadmin > /dev/null 2>>errlog.txt && printf "[${grn}OK${end}]\n" || printf "[${red}NO${end}]\n"; kubectl apply -f ./srcs/phpmyadmin.yaml >> log.log 2>> errlog.txt 
+printf "Building and deploying influxdb:\t"
+docker build -t influxdb_alpine ./srcs/influxdb2 >> /dev/null 2>>errlog.txt && printf "[${grn}OK${end}]\n" || printf "[${red}NO${end}]\n"; kubectl apply -f ./srcs/influxdb.yaml >> log.log 2>> errlog.txt
+printf "Building and deploying grafana:\t\t"
+docker build -t grafana_alpine ./srcs/grafana2 > /dev/null 2>>errlog.txt && printf "[${grn}OK${end}]\n" || printf "[${red}NO${end}]\n"; kubectl apply -f ./srcs/grafana.yaml >> log.log 2>> errlog.txt
+printf "Building and deploying nginx:\t\t"
+docker build -t nginx_alpine ./srcs/nginx > /dev/null 2>>errlog.txt && printf "[${grn}OK${end}]\n" || printf "[${red}NO${end}]\n"; kubectl apply -f ./srcs/nginx.yaml >> log.log 2>> errlog.txt
 
-# kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)" >> log.log 2>> errlog.txt
+open http://$MINIKUBE_IP
 # To enter terminal:
 # kubectl exec -it <podname> sh
